35014692c729424469f685d1621a0f0daee7602e
Daniel Williams <daniellanierwilliams@gmail.com>
2019-08-05 11:52:38 -0500

    transfered to new mern stack to deploy, added connection to mongoose

---
+++ client/build/precache-manifest.862a75a5e346c603cf39b7d432be2ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "515b912719dddc49ab29",
+    "url": "/static/css/main.07315fb7.chunk.css"
+  },
+  {
+    "revision": "515b912719dddc49ab29",
+    "url": "/static/js/main.e16be78f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0cf6c0d759920bc77796",
+    "url": "/static/js/2.1dbdbc6d.chunk.js"
+  },
+  {
+    "revision": "6f629c4ddbfc5611c0e14b572be5b5e6",
+    "url": "/static/media/books-2.6f629c4d.jpg"
+  },
+  {
+    "revision": "04376ec72ebd4452623f8f171dc31d71",
+    "url": "/index.html"
+  }
+];
